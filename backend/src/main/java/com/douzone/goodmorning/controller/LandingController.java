a799b0fd2c914c556cab1f896c9102413f60a4ed
hwimin-kim <gnlals7841@gmail.com>
2022-08-14 19:37:30 +0900

    [Chore] 젠킨스 빌드 파일 추가

---
+++ backend/src/main/java/com/douzone/goodmorning/controller/LandingController.java
@@ -0,0 +1,21 @@
+package com.douzone.goodmorning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LandingController {
+
+	@GetMapping("/favicon.ico")
+	@ResponseBody
+	public void returnNoFavicon() {
+	}
+	
+	@RequestMapping("")
+	public String index() {
+		return "index";
+	}
+
+}
