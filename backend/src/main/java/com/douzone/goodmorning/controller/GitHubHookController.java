22aa004448c7f1123de5443efff8031708df7af6
hwimin-kim <gnlals7841@gmail.com>
2022-09-06 18:54:50 +0900

    t

--- backend/src/main/java/com/douzone/goodmorning/controller/GitHubHookController.java
+++ backend/src/main/java/com/douzone/goodmorning/controller/GitHubHookController.java
@@ -1,18 +1,19 @@
 package com.douzone.goodmorning.controller;
 
+import java.nio.charset.Charset;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
-import org.apache.ibatis.session.SqlSession;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.douzone.goodmorning.dto.Message;
+import com.douzone.goodmorning.dto.status.StatusEnum;
 import com.douzone.goodmorning.service.GithubHookService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,7 +27,7 @@ public class GitHubHookController {
 	private final GithubHookService githubHookService;
 	
 	@PostMapping("/hookdata")
-	public void githubhook(@RequestHeader HashMap<String,Object> headerData,@RequestBody HashMap<String, Object> data){
+	public ResponseEntity<Message> githubhook(@RequestHeader HashMap<String,Object> headerData,@RequestBody HashMap<String, Object> data){
 		
 //		System.out.println(data);
 //		System.out.println(data.values().toArray()[0]);
@@ -55,25 +56,32 @@ public class GitHubHookController {
 //			System.out.println("밸류:" + value);
 //			}
 //		}
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+    	
+    	Message message = new Message();
+    	message.setStatus(StatusEnum.OK);
+    	message.setMessage("success");
+    	message.setData(headerData);
+    	
 		
+//		String key= headerData.get("X-GitHub-Event").toString();
+//		switch(key) {
+//			case "push":
+//				githubHookService.pushEventInsert(data,key);
+//				break;
+//			case "delete":
+//				githubHookService.deleteEventInsert(data,key);
+//				break;
+//			case "create":
+//				githubHookService.createEventInsert(data,key);
+//				break;	
+//			case "pull_request":
+//				githubHookService.pullReqeustEventInsert(data,key);
+//				break;	
+//		}
 		
-		String key= headerData.get("X-GitHub-Event").toString();
-		switch(key) {
-			case "push":
-				githubHookService.pushEventInsert(data,key);
-				break;
-			case "delete":
-				githubHookService.deleteEventInsert(data,key);
-				break;
-			case "create":
-				githubHookService.createEventInsert(data,key);
-				break;	
-			case "pull_request":
-				githubHookService.pullReqeustEventInsert(data,key);
-				break;	
-		}
-		
-		
+		return ResponseEntity.ok().headers(headers).body(message);
 		
 	}
 	
