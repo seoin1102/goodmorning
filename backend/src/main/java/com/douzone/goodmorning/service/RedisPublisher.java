55b00bea0754b10501538e7c43463294fdcce975
hwimin-kim <gnlals7841@gmail.com>
2022-09-07 19:41:13 +0900

    t

--- backend/src/main/java/com/douzone/goodmorning/service/RedisPublisher.java
+++ backend/src/main/java/com/douzone/goodmorning/service/RedisPublisher.java
@@ -3,12 +3,15 @@ package com.douzone.goodmorning.service;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import com.douzone.goodmorning.exception.GlobalExceptionHandler;
 import com.douzone.goodmorning.vo.ChatVo;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class RedisPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
@@ -19,6 +22,7 @@ public class RedisPublisher {
     
     // 2번. 서버 => 레디스로 전달
     public void publish(String topic, ChatVo chatVo) {
+    	log.info("2번 스서버에서 레디스로 전달" + topic + "@" + chatVo);
     	System.out.println("2번 스서버에서 레디스로 전달" + topic + "@" + chatVo);
         redisTemplate.convertAndSend(topic, chatVo);
     }
