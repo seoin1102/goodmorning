a63abdf32a11f8cacf1eb12721ac43eb44e0c636
hwimin-kim <gnlals7841@gmail.com>
2022-09-08 16:41:07 +0900

    github ui 수정

--- frontend/src/components/chat/GitMessageItem.js
+++ frontend/src/components/chat/GitMessageItem.js
@@ -9,20 +9,40 @@ import '../../styles/css/msItem.css';
 function GitMessageItem({align, message, time, name, url}) {
     
 
-    return (
+  return (
         <ListItem key="1" >
             <Grid container spacing={1}>
                 <Grid item xs={0.5} >
                     <ListItemIcon sx={{padding: "7px 0 0 0"}}>
                         <Avatar alt={"null"} src={url} />
                     </ListItemIcon>
-                    <div style={{height: '175px'}}>
-
-                    </div>
+                    <div style={{height: '100px'}}></div>
                 </Grid>
-                <Grid item xs={11.5} style={{width:'max-content', padding:'10px 50px 10px 30px', }} >
-                    <ListItemText align={align} style={{fontWeight:'bolder'}} >{name}  {time}</ListItemText>
-                    <ListItemText align={align} primary={message} sx={{fontFamily:'SUIT-Medium'}} ></ListItemText>
+                <Grid item xs={11.5} style={{width:'max-content', padding:'10px 50px 10px 30px', borderColor: 'black'}} >
+                    <Grid container spacing={1} >
+                        <Grid item xs={12} >
+                            <ListItemText align={align} style={{fontWeight:'bolder'}} >{name}  {time}</ListItemText>
+                        </Grid>
+                        <Grid item xs={0.005} sx={{backgroundColor: 'black', borderRadius: '10px', borderWidth: '1px', padding: '0'}}>
+                            <div style={{height: '65px', padding: '0'}}></div>
+                        </Grid>
+                        <Grid item xs={11.92} >
+                            <Grid item xs={12}>
+                                <ListItemText align={align} primary={'hwimin-kim'} sx={{fontFamily:'SUIT-Medium', padding: '0 0 0 10px'}} ></ListItemText>
+                            </Grid>
+                            <Grid item xs={12} sx={{display: 'flex'}}>      
+                                <div style={{padding: '0 0 0 10px', margin: '0 0 8px 0'}}>{`2 new commit pushed to`}</div>
+                                <div>&nbsp;</div>
+                                <div style={{margin: '0 0 8px 0', backgroundColor: '#e8ebed', border: '1px solid #E1E1E9', color: 'red', borderRadius: '5px'}}>{`master`}</div>    
+                            </Grid>
+                            <Grid item xs={12} sx={{margin: '0px'}}>
+                                <div style={{display: 'flex', justifyContent: 'center'}}>
+                                    <Avatar alt={"null"} src={url} sx={{width: '25px', height: '25px', margin: '0 5px 0 10px', verticalAlign: 'middle'}}/>
+                                    <ListItemText align={align} primary={'goodmorning'}  sx={{fontFamily:'SUIT-Medium'}}/>  
+                                </div>                          
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </ListItem>
