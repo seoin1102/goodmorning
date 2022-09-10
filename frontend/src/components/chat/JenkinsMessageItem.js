8bb384368ff3c41b9282008994cbbba1d7948b0d
hwimin-kim <gnlals7841@gmail.com>
2022-09-10 13:20:51 +0900

    젠킨스 수정...

--- frontend/src/components/chat/JenkinsMessageItem.js
+++ frontend/src/components/chat/JenkinsMessageItem.js
@@ -37,7 +37,7 @@ function JenkinsMessageItem({align, message, time, name, url}) {
         <ListItem key="1" >
             <Grid container spacing={1}>
                 <Grid item xs={0.5} >
-                    <ListItemIcon sx={{padding: "7px 0 0 0"}}>
+                    <ListItemIcon sx={{padding: "7px 0 0 0", width: '45px', height: '45px'}}>
                         <Avatar alt={"null"} src={url} />
                     </ListItemIcon>
                     <div style={{height: '100px'}}></div>
@@ -47,20 +47,26 @@ function JenkinsMessageItem({align, message, time, name, url}) {
                         <Grid item xs={12} >
                             <ListItemText align={align} style={{fontWeight:'bolder'}} >{name}  {time}</ListItemText>
                         </Grid>
-                        <Grid item xs={0.005} sx={{backgroundColor: 'green', borderRadius: '10px', borderWidth: '1px', padding: '0'}}>
+                        <Grid item xs={0.005} sx={messageItem.status === 'SUCCESS' ?
+                                                  {backgroundColor: 'green', borderRadius: '10px', borderWidth: '1px', padding: '0'} :
+                                                  {backgroundColor: 'red', borderRadius: '10px', borderWidth: '1px', padding: '0'}}>
                             <div style={{height: '65px', padding: '0'}}></div>
                         </Grid>
                         <Grid item xs={11.92} >
                             <Grid item xs={12}>
-                                <ListItemText align={align} primary={`${messageItem.buildNumber}번`} sx={{fontFamily:'SUIT-Medium', padding: '0 0 0 10px'}} ></ListItemText>
+                                <ListItemText align={align} primary={`#${messageItem.buildNumber}`} sx={{fontFamily:'SUIT-Medium', padding: '0 0 0 10px'}} ></ListItemText>
                             </Grid>
                             <Grid item xs={12} sx={{display: 'flex'}}>
-                                <div style={{margin: '0 0 8px 0', backgroundColor: '#e8ebed', border: '1px solid #E1E1E9', color: 'blue', borderRadius: '5px'}}>
+                                <div style={{padding: '0 0 0 10px', margin: '0 0 8px 0', backgroundColor: '#e8ebed', border: '1px solid #E1E1E9', color: 'blue', borderRadius: '5px'}}>
                                   {`${messageItem.commit}`}
                                 </div>
-                                <div>&nbsp;</div>  
-                                <div style={{padding: '0 0 0 10px', margin: '0 0 8px 0'}}>
-                                  {`${messageItem.status} after ${Math.floor(messageItem.duration * 0.001 * 100)/100} sec`}
+                                <div>&nbsp;</div>
+                                <div style={{margin: '0 0 8px 0', backgroundColor: '#e8ebed', border: '1px solid #E1E1E9', color: 'yellow', borderRadius: '5px'}}>
+                                  {`${messageItem.status}`}
+                                </div>
+                                <div>&nbsp;</div>
+                                <div style={{margin: '0 0 8px 0'}}>
+                                  {`after ${Math.floor(messageItem.duration * 0.001 * 100)/100} sec`}
                                 </div>                             
                                 <div>&nbsp;</div>
                                 <div style={{margin: '0 0 8px 0'}}>{`to`}</div>
@@ -72,7 +78,7 @@ function JenkinsMessageItem({align, message, time, name, url}) {
                             <Grid item xs={12} sx={{margin: '0px'}}>
                                 <div style={{display: 'flex', justifyContent: 'flex-start'}}>
                                     <a href={messageItem.fullUrl} target={'_blank'} style={{textDecoration: 'none'}}>
-                                        <Avatar alt={"null"} src={url} sx={{width: '25px', height: '25px', margin: '0 5px 0 10px', verticalAlign: 'middle'}}/>
+                                        <Avatar alt={"null"} src={url} sx={{width: '30px', height: '30px', margin: '0 5px 0 10px', verticalAlign: 'middle'}}/>
                                     </a>
                                     <a href={messageItem.fullUrl} target={'_blank'} style={{textDecoration: 'none'}}>
                                         <ListItemText align={align} primary={messageItem.projectName}  sx={{fontFamily:'SUIT-Medium'}}/>
