28e74ed6288b6db3ceed7960d802ded1c067f685
hwimin-kim <gnlals7841@gmail.com>
2022-09-13 16:05:00 +0900

    sdsd

--- frontend/src/components/chat/CommandMessageItem.js
+++ frontend/src/components/chat/CommandMessageItem.js
@@ -5,6 +5,7 @@ import ListItemText from '@mui/material/ListItemText';
 import React from 'react';
 import '../../assets/fonts/font.css';
 import shuttleIcons from '../../assets/icons/shuttle1.png';
+import questionIcons from '../../assets/icons/question1.png';
 import '../../styles/css/msItem.css';
 
 function CommandMessageItem({align, message, time, name, url}) {
@@ -25,7 +26,7 @@ function CommandMessageItem({align, message, time, name, url}) {
                         </Grid>
                         <Grid item xs={3.3} sx={{display: 'flex', borderColor: '#E1E8ED', borderStyle: 'solid', borderWidth: '0 4px 2px 0', borderRadius: '10px', backgroundColor: '#FFFFFF', padding: '0 0 8px 0'}}>
                             <Grid item xs={2} sx={{height: '35px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                <img src={shuttleIcons} width={30} height={30}/>
+                                <img src={message === 'none' ? questionIcons : shuttleIcons} width={30} height={30}/>
                             </Grid>
                             <Grid item xs={10} sx={{margin: ' 0 0 0 5px', display: 'flex', alignItems: 'center'}}>
                                 <div style={{height: '26px', fontFamily:'SUIT-Medium'}}>
