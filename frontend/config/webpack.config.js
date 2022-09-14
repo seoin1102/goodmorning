441b6ea16bd317fd0e913c41ce2d7cd453fc7c7c
김현석 <gustjr324@gmail.com>
2022-09-14 13:34:15 +0900

    ip수정

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -39,8 +39,8 @@ module.exports = function(env) {
             port: 9090,
             proxy: {
               '/api': {target: 'http://localhost:8080'},
-              '/assets': {target: 'http://localhost:8080'},
-              '/html': {target: 'http://localhost:8080'}
+              '/assets': {target: 'http://34.64.235.225:8080'},
+              '/html': {target: 'http://34.64.235.225:8080'}
           },
             liveReload: true,
             hot: false,
--- frontend/src/components/chat/SendPreviewMessage.js
+++ frontend/src/components/chat/SendPreviewMessage.js
@@ -16,7 +16,7 @@ function SendPreviewMessage({align, time, name, message}) {
           gitData[1] = ''
 
       const gitJsonData = JSON.stringify({gitId: gitData[0], projectName: gitData[1]})
-    const metaOG = await linkPreview(`http://localhost:8080/html/github`, gitJsonData);
+    const metaOG = await linkPreview(`http://34.64.235.225:8080/html/github`, gitJsonData);
 
 
       setLinkPreviewMetaOG(metaOG);
